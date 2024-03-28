41b88297e2da1d05c042a5f1c7c31dd4c4eb05fe
Rikpat <kadlcik.patrik@gmail.com>
2024-03-28 11:23:13 +0100

    fixed script

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # if root, change to user specified in PUID env variable
-UID=(id -u)
+UID=$(id -u)
 if [ $UID -eq 0 ]; then
   addgroup -S -g $PGID workgroup
   adduser -S -u $PUID -G workgroup workuser
