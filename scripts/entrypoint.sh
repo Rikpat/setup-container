88be67259e1de48bb12790b896922e5a7480d066
Rikpat <kadlcik.patrik@gmail.com>
2024-03-28 11:20:17 +0100

    added check for root (for securityContext)

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-addgroup -S -g $PGID workgroup
-adduser -S -u $PUID -G workgroup workuser
-su workuser -s /bin/sh -c "$@"
+
+# if root, change to user specified in PUID env variable
+UID=(id -u)
+if [ $UID -eq 0 ]; then
+  addgroup -S -g $PGID workgroup
+  adduser -S -u $PUID -G workgroup workuser
+  su workuser -s /bin/sh -c "$@"
+else
+  /bin/sh -c "$@"
+fi
