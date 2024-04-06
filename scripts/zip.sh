54475275f5cca1b82283bf4e4a37eeef2ea89bd4
Rikpat <kadlcik.patrik@gmail.com>
2024-04-06 11:25:57 +0200

    added option to skip zip name in unpacked directory

--- scripts/zip.sh
+++ scripts/zip.sh
@@ -2,5 +2,8 @@
 mkdir -p $ZIP_DESTINATION_PATH
 for url in $ZIP_URLS; do 
   foldername=$(basename "$url" .zip)
+  if [ -z "${DONT_APPEND_ZIP_NAME}" ]; then
+    foldername=""
+  else
   wget $url -O /tmp/archive.zip && unzip /tmp/archive.zip -o -d $ZIP_DESTINATION_PATH/$foldername; 
 done
